92b62579c46da338e3fae747000dd0f477cca71c
jerrysue9221002 <105869747+jerrysue9221002@users.noreply.github.com>
2022-06-03 18:22:45 +0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
+var colorPick = document.getElementById('colorPicker');
+var table = document.getElementById('pixelCanvas');
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
+var picker = document.getElementById("sizePicker");
 
-// When size is submitted by the user, call makeGrid()
+function makeGrid(height, width){
+    table.innerHTML ="";
+    for(var h = 0; h <= height.value -1; h++){
+        var tr = document.createElement("tr");
+        for(var w = 0; w <= width.value -1; w++){
+            var td = document.createElement("td")
+            tr.appendChild(td);
+        };
+        
+        table.appendChild(tr);
 
-function makeGrid() {
+        tr.addEventListener('click', function (color) {
+            if (color.target.style.backgroundColor){
+                color.target.removeAttribute('style');
+            }else {
+                color.target.style.backgroundColor = colorPick.value;
+            };    
 
-// Your code goes here!
+        });           
+        };
+};
 
-}
+picker.addEventListener('submit', function(event){
+    event.preventDefault();
+    makeGrid(height, width);
+});
