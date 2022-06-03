4fb36563dc46b1c95d2dd61c0a622f2559abba38
jerrysue9221002 <105869747+jerrysue9221002@users.noreply.github.com>
2022-06-03 21:30:26 +0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,29 +4,29 @@ var height = document.getElementById("inputHeight");
 var width = document.getElementById("inputWidth");
 var picker = document.getElementById("sizePicker");
 
-function makeGrid(height, width){
-    table.innerHTML ="";
-    for(var h = 0; h <= height.value -1; h++){
-        var tr = document.createElement("tr");
-        for(var w = 0; w <= width.value -1; w++){
-            var td = document.createElement("td")
-            tr.appendChild(td);
-        };
-        
-        table.appendChild(tr);
+function makeGrid(height, width) {
+	table.innerHTML = "" ;
+	for (var h = 0; h <= height.value - 1; h++) {
+		var tr = document.createElement("tr");
+		for (var w = 0; w <= width.value - 1; w++) {
+			var td = document.createElement("td")
+			tr.appendChild(td);
+		};
 
-        tr.addEventListener('click', function (color) {
-            if (color.target.style.backgroundColor){
-                color.target.removeAttribute('style');
-            }else {
-                color.target.style.backgroundColor = colorPick.value;
-            };    
+		table.appendChild(tr);
 
-        });           
-        };
+		tr.addEventListener('click', function(color) {
+			if (color.target.style.backgroundColor) {
+				color.target.removeAttribute('style');
+			} else {
+				color.target.style.backgroundColor = colorPick.value;
+			};
+
+		});
+	};
 };
 
-picker.addEventListener('submit', function(event){
-    event.preventDefault();
-    makeGrid(height, width);
+picker.addEventListener('submit', function(event) {
+	event.preventDefault();
+	makeGrid(height, width);
 });
